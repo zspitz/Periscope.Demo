a482cdc1bd5b828bf0c342a442e4a819547a423c
Zev Spitz <shivisi@hotmail.com>
2020-06-15 16:25:59 +0300

    VisualizerWindow

---
+++ RandomVisualizer/VisualizerWindowBase.cs
@@ -0,0 +1,16 @@
+﻿using Periscope;
+using RandomVisualizer.Debuggee;
+using System;
+using System.Windows.Input;
+
+namespace RandomVisualizer {
+    public abstract class VisualizerWindowBase : VisualizerWindowBase<VisualizerWindow, Config> {
+        protected override (object windowContext, object optionsContext, Config config) GetViewState(object r, ICommand? OpenInNewWindow) {
+            var response = (Response)r;
+            return (response, response.Config, response.Config);
+        }
+
+        // we don't need this unless we're allowing OpenInNewWindow
+        protected override void TransformConfig(Config config, object parameter) => throw new NotImplementedException();
+    }
+}
